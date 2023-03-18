ad6bfec34820b6d55c2178ad1ff0aae90f112bbf
Steven Lee <admin@alitrack.com>
2023-03-18 23:23:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const crypto = require('crypto');
-
-const nextConfig = {
-  reactStrictMode: true,
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = {
   async rewrites() {
     return [
       {
@@ -15,27 +14,59 @@ const nextConfig = {
       },
     ];
   },
-};
+  async serverMiddleware() {
+    // 创建代理
+    const apiProxy = createProxyMiddleware('/bing', {
+      target: 'https://www.bing.com/:slug*',
+      changeOrigin: true,
+    });
 
-module.exports = nextConfig;
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/bing/:slug*',
-        headers: [
-          {
-            key: 'x-ms-client-request-id',
-            value:  crypto.randomUUID(),
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value:  '*',
-          },          
-          
-        ],
-      },
-    ]
+    // 将代理应用到服务器中间件
+    return (req, res, next) => {
+      if (req.url.startsWith('/bing')) {
+        apiProxy(req, res, next);
+      } else {
+        next();
+      }
+    };
   },
-}
+};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/proxy/:slug*",
+//         destination: "https://api.openai.com/:slug*",
+//       },
+//       {
+//         source: "/bing/:slug*",
+//         destination: "https://www.bing.com/:slug*",
+//       },
+//     ];
+//   },
+// };
+
+
+// module.exports = nextConfig;
+
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         source: '/bing/:slug*',
+//         headers: [
+//           {
+//             key: 'x-ms-client-request-id',
+//             value:  crypto.randomUUID(),
+//           },
+//           {
+//             key: 'Access-Control-Allow-Origin',
+//             value:  '*',
+//           },          
+          
+//         ],
+//       },
+//     ]
+//   },
+// }
